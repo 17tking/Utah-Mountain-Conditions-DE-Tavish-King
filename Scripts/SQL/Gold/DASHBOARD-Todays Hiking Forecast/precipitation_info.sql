9a95555fc45904f1ed95a24b2f08ef3c5265d387
Tavish King <tavishk4@gmail.com>
2026-07-07 21:19:40 -0600

    Adding Precipitation SQL queries

--- Scripts/SQL/Gold/DASHBOARD-Todays Hiking Forecast/precipitation_info.sql
+++ Scripts/SQL/Gold/DASHBOARD-Todays Hiking Forecast/precipitation_info.sql
@@ -1,2 +1,27 @@
--- Getting rain, showers, snow, snow depth, probability info for today
+-- Getting rain, showers, snow, probability info for today
 --
+select 
+      omd.dly_precipitation_probability_mean_pct as mean_precipitation_prob,
+      omd.dly_rain_sum_mm,
+      omd.dly_showers_sum_mm,
+      omd.dly_snowfall_sum_cm,
+      omd.dly_precipitation_sum_mm,
+      omd.dly_precipitation_hours
+from silver.openmeteo_daily as omd
+left join silver.wiki_mtns on wiki_mtns.mtn_id = omd.mtn_id
+where dly_time = current_date
+and {{Mountain}}
+order by omd.mtn_id
+;
+
+-- Hourly Precipitation
+--
+select omh.mtn_id,
+	   to_char(omh.hrly_time, 'HH12 AM') AS hour_time,
+	   omh.hrly_precipitation_probability_pct
+from silver.openmeteo_hourly as omh
+left join silver.wiki_mtns on wiki_mtns.mtn_id = omh.mtn_id
+where date(omh.hrly_time) = current_date
+and {{Mountain}}
+order by omh.mtn_id
+;
